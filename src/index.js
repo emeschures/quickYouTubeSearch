4a5b47efe3731026415bf70941f8a7b989d7363a
Eli Meschures <meschures.eli@gmail.com>
2018-04-27 13:21:42 -0700

    add eventhandler on input field in SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyA7hCoCkWqgTxStNe56oZfkQWnYpLHKVhM'
+const API_KEY = 'AIzaSyA7hCoCkWqgTxStNe56oZfkQWnYpLHKVhM';
 
 // Create a new component. This component should produce some HTML
 const App = () =>  {
@@ -16,4 +16,4 @@ const App = () =>  {
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'));
